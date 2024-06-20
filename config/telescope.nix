443f707533cbacd4b0a36a18f888024cca0865d4
jonwin1 <jonatan.wincent@gmail.com>
2024-06-20 11:01:47 +0200

    fix: telescope breaking change

--- config/telescope.nix
+++ config/telescope.nix
@@ -8,59 +8,55 @@
 
     keymaps = {
       "<leader>sf" = {
-        action = "find_files, {}";
+        action = "find_files";
         options.desc = "[S]earch [F]iles";
       };
       "<leader>sg" = {
-        action = "live_grep, {}";
+        action = "live_grep";
         options.desc = "[S]earch by [G]rep";
       };
-      "<leader>s/" = {
-        action = "live_grep";
-        options.desc = "[S]earch [/] in Files";
-      };
       "<leader><leader>" = {
-        action = "buffers, {}";
+        action = "buffers";
         options.desc = "[ ] Find existing buffers";
       };
       "<leader>sr" = {
-        action = "resume, {}";
+        action = "resume";
         options.desc = "[S]earch [R]esume";
       };
       "<leader>s." = {
-        action = "oldfiles, {}";
+        action = "oldfiles";
         options.desc = "[S]earch Recent Files (\".\" for repeat)";
       };
       "<leader>gf" = {
-        action = "git_files, {}";
+        action = "git_files";
         options.desc = "Search [g]it [f]iles";
       };
       "<leader>gc" = {
-        action = "git_commits, {}";
+        action = "git_commits";
         options.desc = "Search [g]it [c]ommits";
       };
       "<leader>gs" = {
-        action = "git_status, {}";
+        action = "git_status";
         options.desc = "Search [g]it [s]tatus";
       };
       "<leader>/" = {
-        action = "current_buffer_fuzzy_find, {}";
+        action = "current_buffer_fuzzy_find";
         options.desc = "[/] Fuzzily search in current buffer";
       };
       "<leader>sd" = {
-        action = "diagnostics, {}";
+        action = "diagnostics";
         options.desc = "[S]earch [D]iagnostics";
       };
       "<leader>sh" = {
-        action = "help_tags, {}";
+        action = "help_tags";
         options.desc = "[S]earch [H]elp";
       };
       "<leader>sk" = {
-        action = "keymaps, {}";
+        action = "keymaps";
         options.desc = "[S]earch [K]eymaps";
       };
       "<leader>sw" = {
-        action = "grep_string, {}";
+        action = "grep_string";
         options.desc = "[S]earch current [W]ord";
       };
     };
